010df709890669831cf3f96ff488a3084495f9ed
Administrator <Administrator@192.168.3.174>
2017-06-22 16:58:46 -0500

    first commit

---
+++ src/main/java/com/example/demo/RolesUsuarioApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesUsuarioApplication.class, args);
+	}
+}
